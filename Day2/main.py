8c76846ba4ca0d8f22e50cb9621146e5a8fa21d8
JustJie2002 <jiechen067@gmail.com>
2023-07-06 23:56:57 -0400

    Solved Day 2

--- Day2/main.py
+++ Day2/main.py
@@ -14,8 +14,11 @@ class Reader:
     def read(self):
         data = []
         with open(self.fname, "r") as fp:
-            raw_data = fp.read()
-            # TODO
+            raw_data = fp.read().splitlines()
+            for d in raw_data:
+                op, x = d.split()
+                x = int(x)
+                data.append((op, x))
         return data
 
 class Solver:
@@ -23,10 +26,29 @@ class Solver:
         self.data = data
 
     def Part_One(self):
-        return "TBD"
+        xDir, dep = 0, 0
+        for op, x in self.data:
+            if op == "forward":
+                xDir += x
+            elif op == "down":
+                dep += x
+            elif op == "up":
+                dep -= x
+            else: assert False
+        return xDir * dep
 
     def Part_Two(self):
-        return "TBD"
+        xDir, aim, dep = 0, 0, 0
+        for op, x in self.data:
+            if op == "forward":
+                xDir += x
+                dep += aim * x
+            elif op == "down":
+                aim += x
+            elif op == "up":
+                aim -= x
+            else: assert False
+        return xDir * dep
     
 
 if __name__ == "__main__":
