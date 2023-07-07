a9401de549d3dca170aa24fa88c930bd153da5f0
JustJie2002 <jiechen067@gmail.com>
2023-07-06 23:46:43 -0400

    Solved Day 1

--- Day1/main.py
+++ Day1/main.py
@@ -14,19 +14,35 @@ class Reader:
     def read(self):
         data = []
         with open(self.fname, "r") as fp:
-            print(fp)
+            raw_data = fp.read().splitlines()
+            for x in raw_data:
+                x = int(x)
+                data.append(x)
         return data
 
 class Solver:
     def __init__(self, data):
         self.data = data
+        self.n = len(self.data)
 
     def Part_One(self):
-        return "TBD"
+        res = 0
+        for i in range(1, self.n):
+            if self.data[i] > self.data[i - 1]:
+                res += 1
+        return res
 
     def Part_Two(self):
-        return "TBD"
-    
+        pre = [0] * (self.n + 1)
+        for i in range(self.n):
+            pre[i + 1] = pre[i] + self.data[i]
+        def get(i):
+            return pre[i + 1] - pre[i - 2]
+        res = 0
+        for i in range(3, self.n):
+            if get(i) > get(i - 1):
+                res += 1
+        return res
 
 if __name__ == "__main__":
     reader = Reader("input.txt")
